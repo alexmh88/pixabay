4ff5a0c916e4154c0fdddc063ecb1943a5c8f4f1
alexmh88 <alexandra.m.henriksson@gmail.com>
2022-02-12 11:15:43 +0100

    test to see api key and search works

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-let page = 1; 
-let searchQuery= "";
-let searchValue = false;
 
-const pixabayApiKey = "25606330-787c5313477323740e4102695";
-const pixabayUrl = "https://pixabay.com/api/?key="+pixabayApiKey+"&q="+encodeURIComponent('cat');
-const input = document.querySelector('#text-search');
-const color = document.querySelector('#color');
-const searchButton = document.querySelector('#search-button');
-const next = document.querySelector('#next-page');
-const previous = document.querySelector('#previous-page');
+    let page = 1; //default value 1
+    let searchInput= "raccoon"; //test - fix this later
+    let searchValue = false;
+    let color = "black"; //test fix this later
 
-input.addEventListener("#text-search", (x) => {
-    x.preventDefault();
-    searchQuery = x.target.value;
-});
+    const input = document.querySelector('#text-search');
+    const colorOption = document.querySelector('#color');
+    const searchButton = document.querySelector('#search-button');
+    const photogallery = document.querySelector('#display-photo')
+    const next = document.querySelector('#next-page');
+    const previous = document.querySelector('#previous-page');
 
-async function getDataFromPixabay(){
+    input.addEventListener("#text-search", (x) => {
+        x.preventDefault();
+        searchInput = x.target.value;
+    });
     
-    const data = await fetch(pixabayUrl); 
+async function getDataFromPixabay(searchInput, page, color){
+    
+    const pixabayApiKey = '25606330-787c5313477323740e4102695';
+    const pixabayPath = 'https://pixabay.com/api/?';
+    let params = new URLSearchParams(
+        {
+            method: 'GET',
+            content_type: 'application/json',
+            key: pixabayApiKey,           
+            q: searchInput,
+            image_type: 'photo',
+            colors: color,
+            page: page,
+            per_page: 10,
+        }
+    );    
+    const data = await fetch(pixabayPath + params.toString()); 
     const response = await data.json();   //convert the response to json 
-    console.log(response);
+    console.log(response); //comment out this later
     return response;
-}
-getDataFromPixabay();
+    };
 
-
-
-/*const pixabayUrl = 'https://pixabay.com/api';
-const pixabayApiKey = '5606330-787c5313477323740e4102695';
-const searchTerm = 'q'*/
-
-/*let params = new URLSearchParams({
-
-    appid: pixabayApiKey,
-    image_type: "photo",   
-});*/
+getDataFromPixabay(searchInput, page, color); //test fix this later
